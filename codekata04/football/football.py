f897b6e542b71aa6b2e09d1233ea238b8e587b3d
davidnaftalin <davidnaftalin@gmail.com>
2015-08-28 00:29:04 +0100

    Passing test: get_football_data ignores numbering

--- codekata04/football/football.py
+++ codekata04/football/football.py
@@ -2,10 +2,11 @@ def get_football_data(source_data):
     with open(source_data) as f:
         file_data = f.read().splitlines()
     headers = file_data[0].split()
+    start_pos = file_data[0].find(headers[0])
 
     rows = []
     for line in file_data[1:]:
-        row = [convert_value(value) for value in line.split()]
+        row = [convert_value(value) for value in line[start_pos:].split()]
         rows.append(row)
 
     data = [dict(zip(headers, row)) for row in rows]
