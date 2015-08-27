f80f8b280052a8b2e73f15c2ff359823f168fcff
davidnaftalin <davidnaftalin@gmail.com>
2015-08-28 00:24:02 +0100

    Failing test: get_football_data

---
+++ codekata04/tests/test_football.py
@@ -0,0 +1,24 @@
+from football.football import get_football_data
+
+def test_get_football_data():
+    """
+    Parses into list of dicts
+    Parses float, int, str
+    """
+    expected_data = [
+        {
+            'Team': 'Arsenal',
+            'P': 38,
+            'W': 26,
+            'L': 9,
+        },
+        {
+            'Team': 'Liverpool',
+            'P': 38,
+            'W': 24,
+            'L': 8,
+        },
+    ]
+    assert get_football_data(
+        source_data='tests/test_football.dat'
+    ) == expected_data
