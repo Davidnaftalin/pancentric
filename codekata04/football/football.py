a3d5cb5d1cef7c3886c67759fcd09b7a64c54084
davidnaftalin <davidnaftalin@gmail.com>
2015-08-28 00:24:38 +0100

    Passing test: get_football_data

---
+++ codekata04/football/football.py
@@ -0,0 +1,20 @@
+def get_football_data(source_data):
+    with open(source_data) as f:
+        file_data = f.read().splitlines()
+    headers = file_data[0].split()
+
+    rows = []
+    for line in file_data[1:]:
+        row = [convert_value(value) for value in line.split()]
+        rows.append(row)
+
+    data = [dict(zip(headers, row)) for row in rows]
+    return data
+
+def convert_value(value):
+    if value.isalpha():
+        return value
+    if value.isdigit():
+        return int(value)
+    if _isfloat(value):
+        return float(value)
