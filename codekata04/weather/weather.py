05b5057a4e31f0705718824e250d1e78e4c36dd0
davidnaftalin <davidnaftalin@gmail.com>
2015-08-27 23:22:31 +0100

    Passing test: get_weather_data parses type

--- codekata04/weather/weather.py
+++ codekata04/weather/weather.py
@@ -1,12 +1,30 @@
 def get_weather_data(source_data):
     with open(source_data) as f:
         file_data = f.read().splitlines()
+
     headers = file_data[0].split()
-    rows = [
-        [
-        value.strip('*') for value in row.split()
-        ] for row in file_data[1:] if row
-    ]
+    rows = []
+    for line in file_data[1:]:
+        if line:
+            row = line.split()
+            row = [convert_value(value.strip('*')) for value in row]
+            rows.append(row)
 
     data = [dict(zip(headers, row)) for row in rows]
     return data
+
+def _isfloat(string):
+    try:
+        float(string)
+        return True
+    except ValueError:
+        return False
+
+def convert_value(value):
+    if value.isalpha():
+        return value
+    if value.isdigit():
+        return int(value)
+    if _isfloat(value):
+        return float(value)
+
