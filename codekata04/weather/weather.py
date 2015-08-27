9f49216bc88fa83571fb0ac56f3cf62bcbabdc08
davidnaftalin <davidnaftalin@gmail.com>
2015-08-27 22:48:02 +0100

    Passing test: get_weather_data

---
+++ codekata04/weather/weather.py
@@ -0,0 +1,7 @@
+def get_weather_data(source_data):
+    with open(source_data) as f:
+        file_data = f.read().splitlines()
+    headers = file_data[0].split()
+    rows = [row.split() for row in file_data[1:] if row]
+    data = [dict(zip(headers, row)) for row in rows]
+    return data
