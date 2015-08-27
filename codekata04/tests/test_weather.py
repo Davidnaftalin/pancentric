ed6e5036ef174bd6bcaa5ccf4d1f15465e70c011
davidnaftalin <davidnaftalin@gmail.com>
2015-08-27 23:22:12 +0100

    Failing test: get_weather_data parses type

--- codekata04/tests/test_weather.py
+++ codekata04/tests/test_weather.py
@@ -5,17 +5,22 @@ def test_get_weather_data():
     '''
     Parses into list of dicts
     Ignores '*'
+    Parses float, int, str
     '''
     expected_data = [
         {
-        'H1': '1',
-        'H2': '2',
-        'H3': '3',
+        'H1': 1,
+        'H2': 2,
+        'H3': 3,
+        'H4': 1.0,
+        'H5': 'RF',
         },
         {
-        'H1': '4',
-        'H2': '5',
-        'H3': '6',
+        'H1': 4,
+        'H2': 5,
+        'H3': 6,
+        'H4': 12.0,
+        'H5': 'F'
         },
     ]
     assert get_weather_data(source_data='tests/test_weather.dat') == expected_data
