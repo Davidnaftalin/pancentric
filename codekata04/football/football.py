c43aa0b783016e17b0769473c49843d88ded73c0
davidnaftalin <davidnaftalin@gmail.com>
2015-08-28 00:51:59 +0100

    Passing test: min_for_against_diff

--- codekata04/football/football.py
+++ codekata04/football/football.py
@@ -1,5 +1,6 @@
 import re
 
+
 def get_football_data(source_data):
     with open(source_data) as f:
         file_data = f.read().splitlines()
@@ -18,10 +19,22 @@ def get_football_data(source_data):
     data = [dict(zip(headers, row)) for row in rows]
     return data
 
+
 def convert_value(value):
-    if value.isalpha():
-        return value
     if value.isdigit():
         return int(value)
-    if _isfloat(value):
-        return float(value)
+    return value
+
+
+def min_for_against_diff(data):
+    diffs = {
+        row['Team']: max(row['F'], row['A']) - min(row['F'], row['A'])
+        for row in data
+    }
+    return min(diffs, key=diffs.get)
+
+
+if __name__ == '__main__':
+    data = get_football_data('football/football.dat')
+    min_diff_team = min_for_against_diff(data)
+    print min_diff_team
